cf8e8a2ea6aabeea3a1c425c32b48cf3e633e8ce
Felix Forster <fforster1@gmx.de>
2025-01-15 18:39:36 +0100

    Introducing var types date, time, datetime and var fields format, new

--- tests/testthat/test-yaml.R
+++ tests/testthat/test-yaml.R
@@ -1,6 +1,7 @@
-test_that("category values of integer variables are read in correctly", {
-  expect_equal(read_meta(file = test_path("fixtures", "cat_load_test.yml"), testing = T)$main$values[1],"1=one or more|2=two|3=three")
-  expect_equal(read_meta(file = test_path("fixtures", "cat_load_test.yml"), testing = T)$main$values[2],"1=one or more|2=two|3=three")
-  expect_equal(read_meta(file = test_path("fixtures", "cat_load_test.yml"), testing = T)$main$values[3],"1=one or more|2=two|3=three")
-  expect_equal(read_meta(file = test_path("fixtures", "cat_load_test.yml"), testing = T)$main$values[4],"1=one or more|2=two|3=three")
+test_that("yaml.prc.var.list.cats works", {
+  x <- yaml::read_yaml(file = test_path("fixtures", "cat_load_test.yml"))
+  expect_equal(yaml.prc.var.list.cats(x, "cats")$var.list[[1]]$cats,"1=one or more|2=two|3=three")
+  expect_equal(yaml.prc.var.list.cats(x, "cats")$var.list[[2]]$cats,"1=one or more|2=two|3=three")
+  expect_equal(yaml.prc.var.list.cats(x, "cats")$var.list[[3]]$cats,"1=one or more|2=two|3=three")
+  expect_equal(yaml.prc.var.list.cats(x, "cats")$var.list[[4]]$cats,"1=one or more|2=two|3=three")
 })
